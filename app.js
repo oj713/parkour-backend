5b465471c4e817acc5e4979e45e24d218a57a863
Omi Johnson <ojohnson@bigelow.org>
2023-08-19 11:27:02 -0400

    fixed typo that was blocking POST commands

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ mongoose.connect(DB_CONNECTION_STRING)
 
 const app = express();
 
-// app.use(cors({
-//     credentials: true,
-//     origin: process.env.FRONTEND_URL
-// }));
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+}));
 
 const sessionOptions = {
     secret: "any string",
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: false
 };
 if (process.env.NODE_ENV !== "development") {
     sessionOptions.proxy = true;
